db41f42d0180f22970a901ed4cdb11b24d9a6a09
Vegadhardik7 <vegadhardik7@gmail.com>
2025-05-03 16:04:25 +0530

    Clean commit without large files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "WasteDetection",
+    version = "0.0.0",
+    author = "Hardik Vegad",
+    author_email = "hv7479161@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
